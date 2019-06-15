0ef34fbab7fec27667800f6af6e5af13961f0ca8
Arvindeva Wibisono <arvindeva@warungpintar.co>
2019-06-15 15:22:54 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setError(false);
+      setLoading(true);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  return [{ data, loading, error }, setUrl];
+};
+
+const App = () => {
+  const [query, setQuery] = useState('redux');
+  const [{ data, loading, error }, doFetch] = useDataApi(
+    'http://hn.algolia.com/api/v1/search?query=redux',
+    { hits: [] }
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>useEffect data fetch</h1>
       </header>
+      <main>
+        <h2>Hacker News Search</h2>
+        <form
+          onSubmit={event => {
+            doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+            event.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            value={query}
+            onChange={event => setQuery(event.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+        {error && <p>Error</p>}
+        {loading ? (
+          <p>Loading</p>
+        ) : (
+          <ul>
+            {data.hits.map(item => (
+              <li key={item.objectID}>
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
